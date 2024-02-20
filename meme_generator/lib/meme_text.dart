935e452652e5a9356561be5abb536d09d172b4b3
petrukhinandrew <dartmol2300@gmail.com>
2024-02-20 19:01:45 +0300

    done nothing

--- meme_generator/lib/meme_text.dart
+++ meme_generator/lib/meme_text.dart
@@ -5,30 +5,23 @@ class MemeText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final _textController =
-        TextEditingController(text: 'Your meme could be here');
-
-    final _textStyle = const TextStyle(
-      fontFamily: 'Impact',
-      fontSize: 30,
-      color: Colors.white,
-    );
-
-    final node = FocusNode();
-
     return ConstrainedBox(
       constraints: BoxConstraints(
         maxWidth: MediaQuery.of(context).size.width,
         minHeight: 100,
       ),
       child: EditableText(
-        controller: _textController,
-        focusNode: node,
+        controller: TextEditingController(text: 'Your meme could be here'),
+        focusNode: FocusNode(),
         cursorColor: Colors.white,
         maxLines: 3,
         backgroundCursorColor: Colors.white,
         textAlign: TextAlign.center,
-        style: _textStyle,
+        style: const TextStyle(
+          fontFamily: 'Impact',
+          fontSize: 30,
+          color: Colors.white,
+        ),
       ),
     );
   }
