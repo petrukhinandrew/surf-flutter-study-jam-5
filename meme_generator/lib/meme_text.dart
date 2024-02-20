22eac71e25c7716af2282a2bc61a06957603f1eb
petrukhinandrew <dartmol2300@gmail.com>
2024-02-20 19:00:59 +0300

    done nothing

--- meme_generator/lib/meme_text.dart
+++ meme_generator/lib/meme_text.dart
@@ -1,77 +1,34 @@
 import 'package:flutter/material.dart';
 
-class MemeText extends StatefulWidget {
+class MemeText extends StatelessWidget {
   const MemeText({super.key});
 
   @override
-  State<MemeText> createState() => _MemeTextState();
-}
-
-class _MemeTextState extends State<MemeText> {
-  final _textKey = GlobalKey();
-  final _textController = TextEditingController(
-      text: 'Your meme could be here Your meme could be here');
-
-  double _fontSize = 40;
-  final _textStyle = const TextStyle(
-    fontFamily: 'Impact',
-    color: Colors.white,
-  );
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  void _textChanged(String _) {
-    final inputWidth = _textKey.currentContext!.size!.width - 50;
-    final textPainter = TextPainter(
-      textDirection: TextDirection.ltr,
-      text: TextSpan(
-        text: _textController.text,
-        style: const TextStyle(
-          fontFamily: 'Impact',
-          color: Colors.white,
-        ),
-      ),
-    );
-    textPainter.layout();
-
-    double textWidth = textPainter.width;
-    double fontSize = 40;
-
-    while (textWidth > inputWidth && fontSize > 1.0) {
-      fontSize -= 0.5;
-      textPainter.text = TextSpan(
-        text: _textController.text,
-        style: _textStyle.copyWith(fontSize: fontSize),
-      );
-      textPainter.layout();
-      textWidth = textPainter.width;
-    }
-
-    setState(() {
-      _fontSize = fontSize;
-    });
-  }
-
-  final node = FocusNode();
-  @override
   Widget build(BuildContext context) {
+    final _textController =
+        TextEditingController(text: 'Your meme could be here');
+
+    final _textStyle = const TextStyle(
+      fontFamily: 'Impact',
+      fontSize: 30,
+      color: Colors.white,
+    );
+
+    final node = FocusNode();
+
     return ConstrainedBox(
       constraints: BoxConstraints(
         maxWidth: MediaQuery.of(context).size.width,
         minHeight: 100,
       ),
       child: EditableText(
-        key: _textKey,
-        onChanged: _textChanged,
         controller: _textController,
         focusNode: node,
         cursorColor: Colors.white,
-        maxLines: 1,
+        maxLines: 3,
         backgroundCursorColor: Colors.white,
         textAlign: TextAlign.center,
-        style: _textStyle.copyWith(fontSize: _fontSize),
+        style: _textStyle,
       ),
     );
   }
